bf7d7d4cacfa932910eedc9cabe2186bbc11a646
Ana Clara <aniirodferr@gmail.com>
2021-08-12 22:41:42 -0300

    final solution

--- init.js
+++ init.js
@@ -1,27 +1,32 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    //let pokes = [4, 6, 12, 25, 40];
-    let pos = 0;
-  
-    //let poke = pokes[pos];
-    let url = "https://pokeapi.co/api/v2/pokemon/"+4;
-    let imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png";
+     let pokes = [4, 6, 12, 25, 40];
     
-    fetch(url)
-        .then(result => result.json())
-        .then(data => {
-            let ability = data.abilities[0].ability.name;
-            let height = data.height;
-            let weight = data.weight;
-            let name = data.name;
+    for(let i = 0; i < pokes.length; i ++){
+        let pokeNumber = pokes[i];
 
-            document.getElementById("name").textContent = name;
-            document.getElementById("ability").textContent = ability;
-            document.getElementById("height").textContent = height;
-            document.getElementById("weight").textContent = weight;
-            document.getElementById("img").src = imgUrl;
+        let url = "https://pokeapi.co/api/v2/pokemon/"+pokeNumber;
+        let imgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"+pokeNumber+".png";
 
+        fetch(url)
+            .then(result => result.json())
+            .then(data => {
+                let ability = data.abilities[0].ability.name;
+                let height = data.height;
+                let weight = data.weight;
+                let name = data.name;
+
+                let pepito = "";
+
+                pepito += `			
+                    <div class="table-row">		
+                        <div class="table-data" >`+ name +`</div>
+                        <div class="table-data" >${ability}</div>
+                        <div class="table-data" >`+ height +`</div>
+                        <div class="table-data" >${weight}</div>
+                        <div class="table-data"><img src=${imgUrl} id="img"></div>
+                    </div>
+                `
+                document.getElementById("id-el-que-quieres").innerHTML += pepito;
         })
-
-
-
+    }
 });
